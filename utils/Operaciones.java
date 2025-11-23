372bf57d2cd61ba9f05a4f7f93aa6a31d60865a5
MontserratNunez <165596465+MontserratNunez@users.noreply.github.com>
2025-11-22 22:00:10 -0400

    [ADD] Facturacion modelo, controller y DAO

--- utils/Operaciones.java
+++ utils/Operaciones.java
@@ -5,7 +5,7 @@ import java.util.List;
 public interface Operaciones<Clase> {
     boolean insertar(Clase obj);
     boolean editar(Clase obj);
-    boolean eliminar(String id);
-    Clase buscar(String id);
+    boolean eliminar(int id);
+    Clase buscar(int id);
     List<Clase> listar();
 }
